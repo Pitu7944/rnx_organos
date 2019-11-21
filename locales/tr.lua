a4dca77fcf3ea59f076191daf4aaaf5400bd3d9c
CylexVII <54693274+CylexVII@users.noreply.github.com>
2019-11-21 21:35:24 +0300

    tr.lua

---
+++ locales/tr.lua
@@ -0,0 +1,24 @@
+Locales['tr'] = {
+
+    ['press_collect_cerebro'] = 'Beyin toplamak için ~INPUT_CONTEXT~ tuşuna basın.',
+    ['press_collect_corazon'] = 'Kalp toplamak için ~INPUT_CONTEXT~ tuşuna basın.',
+    ['press_collect_medulas'] = 'Kemik iliğini toplamak için ~INPUT_CONTEXT~ tuşuna basın.',
+	['press_collect_instestinos'] = 'Bağırsakları toplamak için ~INPUT_CONTEXT~  tuşuna basın.',
+    ['act_imp_police'] = 'Eylem ~r~başarısız.~s~, ~b~Gerekli olan polis sayısı~s~: ',
+    ['pickup_in_prog'] = '~y~Toplama işlemi devam ediyor.~s~...',
+    ['too_many_pouches'] = 'En fazla 1 vücut taşıyabilirsin..',
+    ['packing_in_prog'] = '~y~İşlem devam ediyor.~s~...',
+    ['no_pouches_sale'] = 'Satabileceğiniz başka bir şey yok.r~s~',
+    ['sale_in_prog'] = '~g~Satış devam ediyor~s~...',
+    ['inv_full_cerebro'] = 'Artık daha fazla toplayamazsınız, envanteriniz dolu',
+    ['inv_full_corezon'] = 'Artık daha fazla toplayamazsınız, envanteriniz dolu',
+    ['inv_full_medulas'] = 'Artık daha fazla toplayamazsınız, envanteriniz dolu',
+    ['inv_full_instestinos'] = 'Artık daha fazla toplayamazsınız, envanteriniz dolu',
+    ['inv_full_huesos'] = 'Artık daha fazla toplayamazsınız, envanteriniz dolu',
+    ['press_collect_huesos'] = 'Kemikleri toplamak için ~INPUT_CONTEXT~ tuşuna basın.',
+    ['press_process_organ'] = 'Gövdeyi dikmek için ~INPUT_CONTEXT~ tuşuna basın.',
+    ['press_sell_body'] = 'Gövdeyi satmak için ~INPUT_CONTEXT~ tuşuna basın.',
+    ['not_enough_organ'] = 'Vücudu dikmek için yeterli sayıda organın yok.',
+    ['sold_one_body'] = 'Bir vücudu sattın.',
+
+}
