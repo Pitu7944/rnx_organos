57e0fdaf7602f1fede8877dd4ab7d34fe8dce2fe
Ren0X1 <34504445+Ren0X1@users.noreply.github.com>
2019-12-07 11:28:51 +0100

    English Translation

---
+++ locales/en.lua
@@ -0,0 +1,24 @@
+Locales['es'] = {
+
+    ['press_collect_cerebro'] = 'Press ~INPUT_CONTEXT~ to collect Brains',
+    ['press_collect_corazon'] = 'Press ~INPUT_CONTEXT~ to collect Hearts',
+    ['press_collect_medulas'] = 'Press ~INPUT_CONTEXT~ to collect Kidneys',
+	['press_collect_instestinos'] = 'Press ~INPUT_CONTEXT~  to collect Livers',
+    ['act_imp_police'] = 'Accion ~r~imposible~s~, ~b~policias~s~: ',
+    ['pickup_in_prog'] = '~y~Digging in Progress~s~...',
+    ['too_many_pouches'] = 'Inventory full.',
+    ['packing_in_prog'] = '~y~Assembly in progress~s~...',
+    ['no_pouches_sale'] = 'You have no more to ~r~sell~s~',
+    ['sale_in_prog'] = '~g~Selling in progress~s~...',
+    ['inv_full_cerebro'] = 'You can no longer collect more, your inventory is ~r~full~s~',
+    ['inv_full_corezon'] = 'You can no longer collect more, your inventory is ~r~full~s~',
+    ['inv_full_medulas'] = 'You can no longer collect more, your inventory is ~r~full~s~',
+    ['inv_full_instestinos'] = 'You can no longer collect more, your inventory is ~r~full~s~',
+    ['inv_full_huesos'] = 'You can no longer collect more, your inventory is ~r~full~s~',
+    ['press_collect_huesos'] = 'Press ~INPUT_CONTEXT~ to collect Bones',
+    ['press_process_organ'] = 'Press ~INPUT_CONTEXT~ to craft a Human Body',
+    ['press_sell_body'] = 'Press ~INPUT_CONTEXT~ to sell a Human Body',
+    ['not_enough_organ'] = 'You dont have the necessary items to craft a Human Body.',
+    ['sold_one_body'] = 'You sold a ~g~Human Body~s~',
+
+}
