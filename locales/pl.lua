db57fb0d3f073e80a32c4cdb6d7555ae95cec9b3
Pitu7944 <Pitu7944@gmail.com>
2020-10-02 10:18:39 +0200

    Update pl.lua

--- locales/pl.lua
+++ locales/pl.lua
@@ -1,1 +1,24 @@
+Locales['pl'] = {
 
+    ['press_collect_cerebro'] = 'Naciśnij ~INPUT_CONTEXT~ aby zbierać Mózgi',
+    ['press_collect_corazon'] = 'Naciśnij ~INPUT_CONTEXT~ aby zbierać Serca',
+    ['press_collect_medulas'] = 'Naciśnij ~INPUT_CONTEXT~ aby zbierać Nerki',
+	  ['press_collect_instestinos'] = 'Naciśnij ~INPUT_CONTEXT~  to collect Livers',
+    ['act_imp_police'] = 'Czynność ~r~Niemożliwa~s~, ~b~Policja w pobliżu~s~: ',
+    ['pickup_in_prog'] = '~y~Trwa Kopanie~s~...',
+    ['too_many_pouches'] = 'Pełen Ekwipunek.',
+    ['packing_in_prog'] = '~y~Trwa Pakowanie~s~...',
+    ['no_pouches_sale'] = 'Nie masz już przedmiotów na ~r~Sprzedaż~s~',
+    ['sale_in_prog'] = '~g~Trwa Sprzedaż~s~...',
+    ['inv_full_cerebro'] = 'Pełen Ekwipunek',
+    ['inv_full_corezon'] = 'Pełen Ekwipunek',
+    ['inv_full_medulas'] = 'Pełen Ekwipunek',
+    ['inv_full_instestinos'] = 'Pełen Ekwipunek',
+    ['inv_full_huesos'] = 'Pełen Ekwipunek',
+    ['press_collect_huesos'] = 'Naciśnij ~INPUT_CONTEXT~ aby zbierać Kości',
+    ['press_process_organ'] = 'Naciśnij ~INPUT_CONTEXT~ aby stworzyc Ciało',
+    ['press_sell_body'] = 'Naciśnij ~INPUT_CONTEXT~ aby sprzedać Ciało',
+    ['not_enough_organ'] = 'Brakuje ci organów aby stworzyć Ciało.',
+    ['sold_one_body'] = 'Sprzedałeś/aś ~g~Ciało~s~',
+
+}
